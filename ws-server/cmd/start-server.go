390eaa4fd4a250dfa35fdadd4e6d8dbada97ce5b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-07-08 23:28:24 +0700

    fix: tidak menggunakan mutex

--- ws-server/cmd/start-server.go
+++ ws-server/cmd/start-server.go
@@ -10,7 +10,6 @@ import (
 	"os/signal"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/lesismal/nbio/nbhttp"
@@ -20,103 +19,28 @@ import (
 )
 
 var (
-	upgrader   = newUpgrader()
-	activeConn *websocket.Conn
-	mu         sync.Mutex
+	upgrader     = newUpgrader()
+	isDebugMode  = false
+	arduinoBoard serial.Port
 )
 
 func newUpgrader() *websocket.Upgrader {
 	u := websocket.NewUpgrader()
 
+	// Keep connection alive for ten minutes
+	u.KeepaliveTime = time.Second * 60 * 10
+
 	u.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
 
-	u.OnClose(func(c *websocket.Conn, err error) {
-		fmt.Println("OnClose:", c.RemoteAddr().String(), err)
-
-		mu.Lock()
-		if activeConn == c {
-			activeConn = nil
-		}
-		mu.Unlock()
-
-	})
-
-	return u
-}
-
-func onWebsocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Upgraded:", conn.RemoteAddr().String())
-}
-
-func StartWebsocketServer(cCtx *cli.Context) error {
-	serverPort := cCtx.Int("port")
-	arduinoPort := cCtx.String("board-port")
-	arduinoBaudRate := cCtx.Int("baud-rate")
-	isDebugMode := cCtx.Bool("debug")
-
-	arduinoMode := &serial.Mode{
-		BaudRate: arduinoBaudRate,
-	}
-
-	if len(arduinoPort) == 0 {
-		fmt.Println("Mohon sebutkan port arduinonya!")
-
-		return nil
-	}
-
-	port, arduErr := serial.Open(arduinoPort, arduinoMode)
-
-	if arduErr != nil {
-		fmt.Println("Terjadi kesalahan dalam menghubungkan papan arduino, Error:", arduErr)
-
-		return nil
-	}
-
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/ws", onWebsocket)
-	engine := nbhttp.NewEngine(nbhttp.Config{
-		Network:                 "tcp",
-		Addrs:                   []string{"127.0.0.1:" + strconv.Itoa(serverPort)},
-		MaxLoad:                 1000000,
-		ReleaseWebsocketPayload: true,
-		Handler:                 mux,
-	})
-
-	engineErr := engine.Start()
-
-	if engineErr != nil {
-		return engineErr
-	}
-
-	upgrader.OnOpen(func(c *websocket.Conn) {
-		if activeConn != nil {
-			fmt.Println("Another client is already connected. Closing the new connection:", c.RemoteAddr().String())
-
-			c.CloseWithError(errors.New("sudah ada client lain yang terhubung"))
-
-			return
-		}
-
-		mu.Lock()
-
-		defer mu.Unlock()
-
-		activeConn = c
-
+	u.OnOpen(func(c *websocket.Conn) {
 		fmt.Println("Client terhubung!")
 
 		buff := make([]byte, 100)
 
 		for {
-			n, err := port.Read(buff)
+			n, err := arduinoBoard.Read(buff)
 
 			if isDebugMode {
 				fmt.Println(n)
@@ -160,6 +84,64 @@ func StartWebsocketServer(cCtx *cli.Context) error {
 			}
 		}
 	})
+
+	u.OnClose(func(c *websocket.Conn, err error) {
+		fmt.Println("OnClose:", c.RemoteAddr().String(), err)
+	})
+
+	return u
+}
+
+func onWebsocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Upgraded:", conn.RemoteAddr().String())
+}
+
+func StartWebsocketServer(cCtx *cli.Context) error {
+	serverPort := cCtx.Int("port")
+	arduinoPort := cCtx.String("board-port")
+	arduinoBaudRate := cCtx.Int("baud-rate")
+	isDebugMode = cCtx.Bool("debug")
+
+	arduinoMode := &serial.Mode{
+		BaudRate: arduinoBaudRate,
+	}
+
+	if len(arduinoPort) == 0 {
+		fmt.Println("Mohon sebutkan port arduinonya!")
+
+		return nil
+	}
+
+	port, arduErr := serial.Open(arduinoPort, arduinoMode)
+	arduinoBoard = port
+
+	if arduErr != nil {
+		fmt.Println("Terjadi kesalahan dalam menghubungkan papan arduino, Error:", arduErr)
+
+		return nil
+	}
+
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/ws", onWebsocket)
+	engine := nbhttp.NewEngine(nbhttp.Config{
+		Network:                 "tcp",
+		Addrs:                   []string{"127.0.0.1:" + strconv.Itoa(serverPort)},
+		MaxLoad:                 1000000,
+		ReleaseWebsocketPayload: true,
+		Handler:                 mux,
+	})
+
+	engineErr := engine.Start()
+
+	if engineErr != nil {
+		return engineErr
+	}
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
